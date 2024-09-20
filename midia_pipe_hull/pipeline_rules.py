ca88d797c95043a1c1dd5394a5c8e000625ade80
matteo <matteo.lacki@gmail.com>
2024-09-20 13:06:45 +0200

    updating pipeline rules and base pipeline like cwazy

--- midia_pipe_hull/pipeline_rules.py
+++ midia_pipe_hull/pipeline_rules.py
@@ -9,28 +9,31 @@ from functools import partial
 
 from snakemaketools.models import Path, RuleOrConfig
 
-
-def register_subconfigs(paths: dict, subconfigs: dict) -> None:
-    for subconfig_type, subconfig in subconfigs.items():
-        assert (
-            "_config" == subconfig_type[-len("_config") :]
-        ), "Subconfig type must end in `_config`"
-        assert (
-            "extension" in subconfig
-        ), f"Each subconfig must specify its extension, e.g. `.toml` or `.json` or `.config`.\nBut `{subconfig_type}` did not."
-    for subconfig_type, subconfig in subconfigs.items():
-        config = RuleOrConfig.GETINSERT(
-            meta={"subconfig": subconfig, "inputs": {}},
-            type=subconfig_type,
-        )
-        paths[subconfig_type] = Path.GETINSERT(
-            path=f"tmp/configs/{subconfig_type}/{config.id}{subconfig['extension']}",
-            type=subconfig_type,
-            rule_or_config=config,
-        )
+# for subconfig_type, subconfig in subconfigs.items():
 
 
-def register_tdf_rawdata(rawdata_tdf):
+def assert_subconfig_is_valid(subconfig_type: str, subconfig: dict) -> None:
+    assert (
+        "_config" == subconfig_type[-len("_config") :]
+    ), "Subconfig type must end in `_config`"
+    assert (
+        "extension" in subconfig
+    ), f"Each subconfig must specify its extension, e.g. `.toml` or `.json` or `.config`.\nBut `{subconfig_type}` did not."
+
+
+def get_subconfig(subconfig_type: str, subconfig: dict) -> Path:
+    config = RuleOrConfig.GETINSERT(
+        meta={"subconfig": subconfig, "inputs": {}},
+        type=subconfig_type,
+    )
+    return Path.GETINSERT(
+        path=f"tmp/configs/{subconfig_type}/{config.id}{subconfig['extension']}",
+        type=subconfig_type,
+        rule_or_config=config,
+    )
+
+
+def register_tdf_rawdata(rawdata_tdf: str) -> tuple[Path, Path, Path]:
     rule = RuleOrConfig.GETINSERT(
         meta=dict(rawdata_tdf=rawdata_tdf, inputs={}),
         type="register_tdf_rawdata",
@@ -53,7 +56,7 @@ def register_tdf_rawdata(rawdata_tdf):
     return folder_d, analysis_tdf, analysis_tdf_bin
 
 
-def register_fasta(fasta):
+def register_fasta(fasta: str) -> Path:
     rule = RuleOrConfig.GETINSERT(
         meta=dict(fasta=fasta, inputs={}),
         type="register_fasta",
@@ -80,23 +83,168 @@ def hash256(path: Path) -> Path:
     return hashfile
 
 
+def remove_rawdata_baseline(
+    raw_data: Path,
+    config: Path,
+) -> tuple[Path, Path, Path]:
+    assert raw_data.id != None
+    assert raw_data.type == "raw_data"
+    assert config.id != None
+    assert config.type == "baseline_removal_config"
+
+    rule = RuleOrConfig.GETINSERT(
+        meta=dict(
+            inputs={
+                "raw_data": raw_data.id,
+                "config": config.id,
+            }
+        ),
+        type="remove_rawdata_baseline",
+    )
+    folder_d = Path.GETINSERT(
+        path=f"tmp/spectra/no_baseline/{rule.id}.d",
+        type="raw_data",
+        rule_or_config=rule,
+    )
+    analysis_tdf = Path.GETINSERT(
+        path=f"{folder_d.path}/analysis.tdf",
+        type="sqlite",
+        rule_or_config=rule,
+    )
+    analysis_tdf_bin = Path.GETINSERT(
+        path=f"{folder_d.path}/analysis.tdf_bin",
+        type="tdf_bin",
+        rule_or_config=rule,
+    )
+    return folder_d, analysis_tdf, analysis_tdf_bin
+
+
+def report_if_dataset_and_calibration_comply(
+    dataset: Path,
+    calibration: Path,
+) -> Path:
+    rule = RuleOrConfig.GETINSERT(
+        meta=dict(
+            inputs={
+                "dataset": dataset.id,
+                "calibration": calibration.id,
+            }
+        ),
+        type="dataset_matches_calibration_assertion",
+    )
+    assertion = Path.GETINSERT(
+        path=f"tmp/assertions/dataset_matches_calibration/{rule.id}.d",
+        type="dataset_matches_calibration_assertion",
+        rule_or_config=rule,
+    )
+    return assertion
+
+
+def raw_data_marginals_plots_folder(raw_data: Path) -> Path:
+    assert raw_data.id != None
+    assert raw_data.type == "raw_data"
+
+    rule = RuleOrConfig.GETINSERT(
+        meta=dict(
+            inputs={
+                "raw_data": raw_data.id,
+            }
+        ),
+        type="raw_data_marginals_plots_folder",
+    )
+    plots_of_marginal_distributions = Path.GETINSERT(
+        path=f"tmp/raw_data_marginals/{rule.id}.d",
+        type="raw_data_marginals_plots_folder",
+        rule_or_config=rule,
+    )
+    return plots_of_marginal_distributions
+
+
+def cluster_with_tims(
+    dataset: Path, config: Path, level: str
+) -> tuple[Path, Path, Path]:
+    assert dataset.id != None
+    assert dataset.type == "raw_data"
+    assert config.id != None
+    assert config.type == f"{level}_clustering_config"
+
+    rule = RuleOrConfig.GETINSERT(
+        meta=dict(
+            inputs={
+                "dataset": dataset.id,
+            }
+        ),
+        type=f"cluster_{level}s_with_tims",
+    )
+    precursor_clusters_hdf = Path.GETINSERT(
+        path=f"tmp/{level}_clusters/tims/{rule.id}.hdf",
+        type=f"{level}_clusters_hdf",
+        rule_or_config=rule,
+    )
+    precursor_clustering_stdout = Path.GETINSERT(
+        path=f"tmp/{level}_clusters/tims/{rule.id}.stdout",
+        type=f"tims_{level}_clusters_stdout",
+        rule_or_config=rule,
+    )
+    precursor_clustering_stderr = Path.GETINSERT(
+        path=f"tmp/{level}_clusters/tims/{rule.id}.stderr",
+        type=f"tims_{level}_clusters_stderr",
+        rule_or_config=rule,
+    )
+
+    return (
+        precursor_clusters_hdf,
+        precursor_clustering_stdout,
+        precursor_clustering_stderr,
+    )
+
+
+cluster_precursors_with_tims = partial(cluster_with_tims, level="precursor")
+cluster_fragments_with_tims = partial(cluster_with_tims, level="fragment")
+
+
+def postprocess_tims_clusters(
+    clusters_hdf: Path,
+) -> tuple[Path, Path]:
+    return clusters, additional_cluster_stats
+
+
+def postprocess_precursor_tims_clusters(
+    precursor_clusters_hdf: Path,
+) -> tuple[Path, Path]:
+    return precursor_clusters, additional_precursor_cluster_stats
+
+
+def postprocess_fragments_tims_clusters(
+    fragment_clusters_hdf: Path,
+) -> tuple[Path, Path]:
+    return fragment_clusters, additional_fragment_cluster_stats
+
+
 # def cluster(
-#     raw_data: Node,
-#     config: Node,
+#     raw_data: Path,
+#     config: Path,
 #     type: str,
 #     rule: str,
-# ) -> tuple[Node, Node, Node]:
+# ) -> tuple[Path, Path, Path]:
 #     for arg in (raw_data, config):
 #         assert arg.id is not None
 
-#     _origin = dict(
-#         rule=rule,
-#         inputs=dict(raw_data=raw_data.id, config=config.id),
+#     rule = RuleOrConfig.GETINSERT(
+#         meta=dict(
+#             inputs={
+#                 "raw_data": raw_data.id,
+#                 "config": config.id,
+#             }
+#         ),
+#         type=type,
 #     )
 
-#     data = Node.GETINSERT(origin=_origin, type=type, extension=".startrek")
-#     stdout = Node.GETINSERT(origin=_origin, type="stdout", extension=".txt")
-#     stderr = Node.GETINSERT(origin=_origin, type="stderr", extension=".txt")
+#     data = Path.GETINSERT(
+#         path=
+#         origin=_origin, type=type, extension=".startrek")
+#     stdout = Path.GETINSERT(origin=_origin, type="stdout", extension=".txt")
+#     stderr = Path.GETINSERT(origin=_origin, type="stderr", extension=".txt")
 
 #     return data, stdout, stderr
 
@@ -105,7 +253,7 @@ def hash256(path: Path) -> Path:
 # cluster_fragments = partial(cluster, type="fragment_clusters")
 
 
-# def get_cluster_stats(clusters: Node, config: Node) -> Node:
+# def get_cluster_stats(clusters: Path, config: Path) -> Path:
 #     for arg in (clusters, config):
 #         assert arg.id is not None
 
@@ -114,30 +262,13 @@ def hash256(path: Path) -> Path:
 #         inputs=dict(clusters=clusters.id, config=config.id),
 #     )
 
-#     cluster_stats = Node.GETINSERT(origin=_origin, type="cluster_stats")
+#     cluster_stats = Path.GETINSERT(origin=_origin, type="cluster_stats")
 #     return cluster_stats
 
 
-# def remove_rawdata_baseline(raw_data: Node, config: Node) -> Node:
-#     for arg in (raw_data, config):
-#         assert arg.id is not None
-
-#     _origin = dict(
-#         rule="remove_rawdata_baseline",
-#         inputs=dict(dataset=raw_data.id, config=config.id),
-#     )
-
-#     raw_data_without_baseline = Node.GETINSERT(
-#         origin=_origin,
-#         type="raw_data_without_baseline",
-#         extension=".d",
-#     )
-#     return raw_data_without_baseline
-
-
 # def match_precursors_and_fragments(
-#     precursor_stats: Node, fragment_stats: Node, matching_config: Node
-# ) -> Node:
+#     precursor_stats: Path, fragment_stats: Path, matching_config: Path
+# ) -> Path:
 #     for arg in (precursor_stats, fragment_stats, matching_config):
 #         assert arg.id is not None
 
@@ -150,6 +281,6 @@ def hash256(path: Path) -> Path:
 #         ),
 #     )
 
-#     rough_matches = Node.GETINSERT(origin=_origin, type="rough_matches.startrek")
+#     rough_matches = Path.GETINSERT(origin=_origin, type="rough_matches.startrek")
 
 #     return rough_matches
--- midia_pipe_hull/pipelines/base.py
+++ midia_pipe_hull/pipelines/base.py
@@ -15,44 +15,96 @@ def pipeline(
     # defaults
     calibration: str = "",  # "" == using Bruker windows
 ) -> DotDict:
+    """
+    Remember that here we only register paths in the DB.
+    That does not automatically mean that all of these paths will ever be created.
+    """
     paths = DotDict()
-    register_subconfigs(paths, subconfigs)
+
+    for subconfig_type, subconfig in subconfigs.items():
+        assert_subconfig_is_valid(subconfig_type, subconfig)
+
+    for subconfig_type, subconfig in subconfigs.items():
+        paths[subconfig_type] = get_subconfig(
+            subconfig_type,
+            subconfig,
+        )
+
+    paths.fasta = register_fasta(fasta)
 
     paths.dataset, paths.dataset_tdf, paths.dataset_tdf_bin = register_tdf_rawdata(
         dataset
     )
-    (
-        paths.calibration,
-        paths.calibration_tdf,
-        paths.calibration_tdf_bin,
-    ) = register_tdf_rawdata(calibration)
-
-    paths.fasta = register_fasta(fasta)
-
     paths.dataset_analysis_tdf_hash = hash256(paths.dataset_tdf)
     paths.dataset_analysis_tdf_bin_hash = hash256(paths.dataset_tdf_bin)
-    paths.calibration_analysis_tdf_hash = hash256(paths.calibration_tdf)
-    paths.calibration_analysis_tdf_bin_hash = hash256(paths.calibration_tdf_bin)
+    paths.dataset_marginals_plots = raw_data_marginals_plots_folder(paths.dataset)
 
-    # if "config_baseline_removal" in subconfigs:
-    #     paths.dataset = remove_rawdata_baseline(
-    #         paths.dataset, paths.config_baseline_removal
-    #     )
+    if calibration:
+        (
+            paths.calibration,
+            paths.calibration_tdf,
+            paths.calibration_tdf_bin,
+        ) = register_tdf_rawdata(calibration)
 
-    # (
-    #     paths.precursors,
-    #     paths.precursor_clustering_stdout,
-    #     paths.precursor_clustering_stderr,
-    # ) = cluster_precursors(paths.dataset, paths.precursor_clustering_config)
+        paths.calibration_analysis_tdf_hash = hash256(paths.calibration_tdf)
+        paths.calibration_analysis_tdf_bin_hash = hash256(paths.calibration_tdf_bin)
 
-    # (
-    #     paths.fragments,
-    #     paths.fragment_clustering_stdout,
-    #     paths.fragment_clustering_stderr,
-    # ) = cluster_fragments(paths.dataset, paths.fragment_clustering_config)
+        paths.dataset_matches_calibration_assertion = (
+            report_if_dataset_and_calibration_comply(
+                dataset=paths.dataset,
+                calibration=paths.calibration,
+            )
+        )
+        paths.calibration_marginals_plots = raw_data_marginals_plots_folder(
+            paths.calibration
+        )
+
+    if "baseline_removal_config" in subconfigs:
+        (
+            paths.dataset,
+            paths.dataset_analysis_tdf,
+            paths.dataset_analysis_tdf_bin,
+        ) = remove_rawdata_baseline(
+            dataset=paths.dataset,
+            config=paths.baseline_removal_config,
+        )
+
+    if subconfigs["precursor_clustering_config"]["software"] == "tims":
+        (
+            paths.precursor_clusters_hdf,
+            paths.precursor_clustering_stdout,
+            paths.precursor_clustering_stderr,
+        ) = cluster_precursors_with_tims(
+            dataset=paths.dataset,
+            config=paths.precursor_clustering_config,
+        )
+        (
+            paths.precursor_clusters,
+            paths.additional_precursor_cluster_stats,
+        ) = postprocess_fragment_tims_clusters(
+            clusters_hdf=paths.precursor_clusters_hdf,
+            analysis_tdf=paths.dataset_analysis_tdf,
+        )
+
+    if subconfigs["fragment_clustering_config"]["software"] == "tims":
+        (
+            paths.fragment_clusters_hdf,
+            paths.fragment_clustering_stdout,
+            paths.fragment_clustering_stderr,
+        ) = cluster_fragments_with_tims(
+            dataset=paths.dataset,
+            config=paths.fragment_clustering_config,
+        )
+        (
+            paths.fragment_clusters,
+            paths.additional_fragment_cluster_stats,
+        ) = postprocess_fragment_tims_clusters(
+            clusters_hdf=paths.fragment_clusters_hdf,
+            analysis_tdf=paths.dataset_analysis_tdf,
+        )
 
     # paths.precursor_stats = get_cluster_stats(
-    #     paths.precursors,
+    #     precursor_clusters=paths.precursors,
     #     paths.precursor_cluster_stats_config,
     # )
 
